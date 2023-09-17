73683808169c6af2bc6e3f528efea12bffaa8f28
Nada ahmed <106588334+NadaAly56@users.noreply.github.com>
2023-09-17 06:37:56 +0300

    Delete backend directory

--- backend/database/dbConnection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-export const dbConnection = ()=> {
-    mongoose.connect(process.env.DB_CONNECTION).then(()=>{
-    console.log("DB Connected");
-}).catch((err)=>{
-    console.log(err);
-});
-} 
--- backend/src/utiles/generateToken.js
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export const generateToken = (payload)=> {
-    const token = jwt.sign(payload, process.env.JWT_KEY)
-    return token;
-}
